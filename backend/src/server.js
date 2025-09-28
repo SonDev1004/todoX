8adf1c3e654d1a7ab1a7e28ae7c35a20b0424a24
SonDev <son.lyhoang2014@gmail.com>
2025-09-28 08:10:58 +0000

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,38 @@ import taskRouters from "./routes/taskRouters.js";
 import {connectDB} from "../config/db.js";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // directoryName
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({origin: ['http://localhost:5173', 'http://localhost:5174']}));
+
+// Cấu hình CORS chỉ chạy khi môi trường development
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors({origin: ['http://localhost:5173', 'http://localhost:5174']}));
+}
+
 app.use('/api/task', taskRouters)
 
-connectDB().then(()=>{
+console.log("🚀 ~  ~ process.env.NODE_ENV: ", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    // Với bất kỳ URL nào mà người dùng gõ vào trình duyêt. Nó sẽ trả về file index.html
+    // để React Router xử lý định tuyến phía client
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
+
+connectDB().then(() => {
 
     app.listen(PORT, () => {
         console.log('Server đang chạy trên cổng 5001');
